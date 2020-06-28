cb23283e54d4d7c5821527d926731ca9b8693d03
Sebastian <sebastianscioli@gmail.com>
2020-06-28 11:05:56 -0300

    Mejora de splash: visualización y tiempo Reformato de AndroidManifest.xml

--- app/src/main/java/com/seba/testml/ui/SearchActivity.java
+++ app/src/main/java/com/seba/testml/ui/SearchActivity.java
@@ -2,15 +2,26 @@ package com.seba.testml.ui;
 
 import android.os.Bundle;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import com.seba.testml.R;
 
 public class SearchActivity extends BaseActivity {
 
+    private static final int SPLASH_TIME = 2500; // ms
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        keepSplash();
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
     }
+
+    private void keepSplash() {
+        try {
+            Thread.sleep(SPLASH_TIME);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
